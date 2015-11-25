6aef2ca5ba43d962584a1bffed8c7ed215b438c6
Rebecca Tolmach <rmtolmach@gmail.com>
2015-11-25 17:18:02 -0500

    created a new and show page for memories. person_id information is not being transmitted

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,4 +4,21 @@ class MemoriesController < ApplicationController
     @person = Person.find(params[:person_id])
     @memory = Memory.new
   end
+
+  def create
+    Memory.create(memory_params)
+
+    redirect_to person_memory_path(params[:person_id], [:id])
+  end
+
+  def show
+    @memory = Memory.all
+  end
+
+################ PRIVADO! #################
+ def memory_params
+   params.require(:memory).permit(:memory, :author)
+ end
+
+
 end
