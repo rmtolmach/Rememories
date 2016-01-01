a8857a8062a0d2c6b99e4fb29172c4cc556bfb26
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-01 14:57:15 -0800

    added specs for memories model

--- spec/models/memory_spec.rb
+++ spec/models/memory_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Memory, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    required_fields = [:description, :author, :person_id]
+    let (:bad_memory){Memory.new}
+
+    required_fields.each do |field|
+      it "must have a #{field}" do
+          expect(bad_memory).to_not be_valid
+          expect(bad_memory.errors.keys).to include(field)
+      end
+    end
+  end
 end
